65e97a421452c54484bf649192ca17c11f2b32ac
Harshit Luthra <love.coolsachin@gmail.com>
2017-10-03 17:57:48 +0530

    Patched Toolbar

--- app/src/main/java/luthra/harshit/phonenumberauthwrtfireb0/SignIn.java
+++ app/src/main/java/luthra/harshit/phonenumberauthwrtfireb0/SignIn.java
@@ -1,45 +1,45 @@
 package luthra.harshit.phonenumberauthwrtfireb0;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class SignIn extends AppCompatActivity {
-TextView txt;
+    TextView txt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-            txt=(TextView)findViewById(R.id.txtPhone);
-        if (getIntent()!=null){
+        txt = findViewById(R.id.txtPhone);
+        if (getIntent() != null) {
             txt.setText(getIntent().getStringExtra("phone"));
-            Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+            Toolbar toolbar = findViewById(R.id.toolbar1);
             toolbar.setTitle("FireBase Auth");
-            setSupportActionBar(toolbar);
+
         }
 
-            }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
 
-return true;
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId()==R.id.action_sign_out){
+        if (item.getItemId() == R.id.action_sign_out) {
             signOut();
 
         }
@@ -50,7 +50,7 @@ return true;
         AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                startActivity(new Intent(SignIn.this,MainActivity.class));
+                startActivity(new Intent(SignIn.this, MainActivity.class));
                 finish();
             }
         });
