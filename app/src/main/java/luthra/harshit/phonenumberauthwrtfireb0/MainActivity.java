cbd4abb73d5ae78ebe757d0d090901da1eb3ead3
Harshit Luthra <love.coolsachin@gmail.com>
2017-10-03 13:31:23 +0530

    Initial and  Final commit

---
+++ app/src/main/java/luthra/harshit/phonenumberauthwrtfireb0/MainActivity.java
@@ -0,0 +1,72 @@
+package luthra.harshit.phonenumberauthwrtfireb0;
+
+import android.content.Intent;
+import android.net.Network;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseAuth auth= FirebaseAuth.getInstance();
+        // If Already  Logged in
+        if (auth.getCurrentUser()!=null)
+        {
+            if(!FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber().isEmpty()){
+                startActivity(new Intent(MainActivity.this,SignIn.class).putExtra("phone",FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber().isEmpty()));
+                finish();
+            }
+        }
+        else {
+           startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build())).build(),1000);
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1000) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            //Successfully Signed in
+            if (resultCode==RESULT_OK)
+            {
+                if (!FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber().isEmpty()) {
+                    startActivity(new Intent(this, SignIn.class).putExtra("phone", FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()));
+                    finish();
+                    return;
+                }
+                //Sign in Failed
+                else {
+                    if (response==null) {
+                        Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                   if (response.getErrorCode()== ErrorCodes.NO_NETWORK){
+                            Toast.makeText(this,"No internet",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                if (response.getErrorCode()== ErrorCodes.UNKNOWN_ERROR){
+                    Toast.makeText(this,"Unknown Error",Toast.LENGTH_SHORT).show();
+                return;}
+            }
+
+                }
+            }
+        }
+
+    }
+
---
+++ app/src/main/java/luthra/harshit/phonenumberauthwrtfireb0/SignIn.java
@@ -0,0 +1,59 @@
+package luthra.harshit.phonenumberauthwrtfireb0;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class SignIn extends AppCompatActivity {
+TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+            txt=(TextView)findViewById(R.id.txtPhone);
+        if (getIntent()!=null){
+            txt.setText(getIntent().getStringExtra("phone"));
+            Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+            toolbar.setTitle("FireBase Auth");
+            setSupportActionBar(toolbar);
+        }
+
+            }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.action_sign_out){
+            signOut();
+
+        }
+        return true;
+    }
+
+    private void signOut() {
+        AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                startActivity(new Intent(SignIn.this,MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+}
